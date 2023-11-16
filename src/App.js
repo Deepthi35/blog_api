65d6cbbc8c2434c03a326dbf8b174b2be31e0aa1
Deepthi35 <deepthimadhabattula@gmail.com>
2023-11-16 18:03:44 +0530

    folder is uploaded

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import './components/post/post.css'
+import Posts from "./components/Posts/Posts";
+
+function App() {
+  return (
+  
+    <div className="main-container">
+      <h1 className="main-heading">
+        Blog App using React Js 
+      </h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
+
+  
+
